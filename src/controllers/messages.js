fa655a6fd3fce51902d5d62caf8c9447dfde4c6f
Tisks <leiser.mahu@usach.cl>
2021-05-06 13:07:49 -0400

    Print data

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -5,6 +5,7 @@ const messagesModel = new Model('messages');
 export const messagesPage = async (req, res) => {
   try {
     const data = await messagesModel.select('name, message');
+    console.log(data)
     res.status(200).json({ messages: data.rows });
   } catch (err) {
     res.status(200).json({ messages: err.stack });
