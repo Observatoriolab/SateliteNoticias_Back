20a2a3ac29774ecd158179c83af65ed8bc39d9dc
Tisks <leiser.mahu@usach.cl>
2021-05-19 22:34:43 -0400

    Adding addnewuser endpoint function

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,14 +3,9 @@ import Model from '../models/model';
 const userModel = new Model('User');
 
 export const addNewUser = async (req, res) => {
-  const {title, content_summary, content_full, source, axis_primary, axis_secondary, 
-        country, bibliography, rating_relevance, rating_relevance_accum, rating_irrelevance, rating_irrelevance_accum,
-        authors, author_count, created_at, date, voter_count, slug} = req.body;
-  const columns = 'title, content_summary, content_full, source, axis_primary, axis_secondary, country, bibliography, rating_relevance, rating_relevance_accum, rating_irrelevance, rating_irrelevance_accum, authors, author_count, created_at, date, voter_count, slug';
-  const values = `'${title}', '${content_summary}', '${content_summary}', '${content_full}', '${source}'
-                '${axis_primary}', '${axis_secondary}', '${country}', '${bibliography}', '${rating_relevance}'
-                '${rating_relevance_accum}', '${rating_irrelevance}', '${rating_irrelevance_accum}', '${authors}', '${author_count}'
-                '${created_at}', '${date}', '${voter_count}', '${slug}'`;
+  const {name, password, email, created_at} = req.body;
+  const columns = 'name, password, email, created_at';
+  const values = `'${name}', '${password}', '${email}', '${created_at}'`;
   try {
     const data = await userModel.insertWithReturn(columns, values);
     res.status(200).json({ messages: data.rows });
@@ -18,4 +13,3 @@ export const addNewUser = async (req, res) => {
     res.status(200).json({ messages: err.stack });
   }
 };
-
