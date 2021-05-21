6f6b234b39a42a1d86efb5d8319ba559045a8a97
Tisks <leiser.mahu@usach.cl>
2021-05-21 17:48:16 -0400

    Adding new user routes

---
+++ src/routes/user.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {addNewUser, loginUser} from '../controllers';
+
+const userRouter = express.Router();
+userRouter.post('/login', loginUser);
+userRouter.post('/registration', addNewUser);
+
+export default editionRouter;
