049fec56b15573a1a69e8a9e32b6892cd01574a7
Tisks <leiser.mahu@usach.cl>
2021-05-21 17:16:46 -0400

    Cors added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import logger from 'morgan';
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
 
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
