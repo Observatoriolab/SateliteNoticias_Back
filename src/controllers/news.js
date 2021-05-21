636c97fd51a741bec851000f42c06f3c3eb702a7
Tisks <leiser.mahu@usach.cl>
2021-05-21 17:38:39 -0400

    date got by toLocaleDateString function to post in db

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -20,13 +20,16 @@ export const getBatchNews = async (req, res) => {
 };
 
 export const addPieceNews = async (req, res) => {
-  const {title, content_summary, content_full, source, axis_primary, axis_secondary, 
-        country, created_at, date, slug} = req.body;
+  const {title, content, source, axis_primary, axis_secondary, 
+        country, date, slug} = req.body;
   
-  const columns = 'title, content_summary, content_full, source, axis_primary, axis_secondary, country, bibliography, rating_relevance, rating_relevance_accum, rating_irrelevance, rating_irrelevance_accum, authors, author_count, created_at, date, voter_count_relevance, voter_count_irrelevance, slug';
-  const values = `'${title}', '${content_summary}', '${content_summary}', '${content_full}', '${source}'
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+  const created_at = today.toLocaleDateString(); // "6/14/2020"
+  const columns = 'title, content_summary, source, axis_primary, axis_secondary, country, bibliography, rating_relevance, rating_relevance_accum, rating_irrelevance, rating_irrelevance_accum, authors, author_count, created_at, date, voter_count_relevance, voter_count_irrelevance, slug';
+  const values = `'${title}', '${content}','${source}'
                 '${axis_primary}', '${axis_secondary}', '${country}', '${nullValue}','${zero}'
-                '${zero}', '${zero}', '${zero}', '', '${zero}',
+                '${zero}', '${zero}', '${zero}', '${nullValue}', '${zero}',
                 '${created_at}', '${date}', '${zero}','${zero}', '${slug}'`;
   try {
     const data = await newsModel.insertWithReturn(columns, values);
