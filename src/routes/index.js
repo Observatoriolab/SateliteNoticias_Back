2a9fa4bd6a92487188925c2aef8f70aa320a39d4
Tisks <leiser.mahu@usach.cl>
2021-05-21 17:48:58 -0400

    Adding user routes to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,14 @@ import newsRouter from './news';
 import editionRouter from './edition';
 import commentRouter from './comment';
 import messageRouter from './message';
+import userRouter from './user';
+
 
 indexRouter.use(newsRouter)
 indexRouter.use(editionRouter)
 indexRouter.use(commentRouter)
 indexRouter.use(messageRouter)
+indexRouter.use(userRouter)
 
 
 export default indexRouter;
