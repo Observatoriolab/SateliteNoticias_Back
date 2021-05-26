0d942e3a591ae2f93df249bf5a43729f9e15f1af
Obstechlab <obstechlab@gmail.com>
2021-05-26 02:44:07 -0400

    Agrego cambio del commit Cros added de Experimental al master

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
+import cors from 'cors';
 
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(indexRouter);
